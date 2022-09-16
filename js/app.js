253b0c4766b892d6683eeadc518076379edeb5f6
Yamil Escobar <joseyamilej@hotmail.com>
2022-09-16 23:15:36 +0200

    Validacion y Mostrar un mensaje de error

--- js/app.js
+++ js/app.js
@@ -10,6 +10,50 @@ const sintomasInput = document.querySelector('#sintomas');
 const formulario = document.querySelector('#nueva-cita');
 const contenedorCitas = document.querySelector('#citas');
 
+
+class Citas {
+    constructor(){
+        this.citas = [];
+    }
+}
+
+class UI {
+
+    imprimirAlerta(mensaje, tipo ){
+        // Crear el div
+        const divMensaje = document.createElement('div');
+        divMensaje.classList.add('text-center', 'alert', 'd-block', 'col-12');
+
+        // Agregar clase en base al tipo de error
+        if(tipo === 'error') {
+            divMensaje.classList.add('alert-danger');
+        } else {
+            divMensaje.classList.add('alert-success');
+        }
+
+        // Mensaje de error
+        divMensaje.textContent = mensaje;
+
+        // Agregar al DOM
+        document.querySelector('#contenido').insertBefore(divMensaje, document.querySelector('.agregar-cita'));
+
+        // Quitar la alerta después de 5 segundos
+        setTimeout( () => {
+            divMensaje.remove();
+        }, 5000);
+
+
+    }
+
+}
+
+
+
+const ui = new UI();
+const administarCitas = new Citas();
+
+
+
 //Registrar eventos
 eventListeners();
 function eventListeners() {
@@ -19,6 +63,8 @@ function eventListeners() {
     fechaInput.addEventListener('input', datosCita);
     horaInput.addEventListener('input', datosCita);
     sintomasInput.addEventListener('input', datosCita);
+
+    formulario.addEventListener('submit', nuevaCita);
 }
 
 // Objeto con la infroamcion de la cita
@@ -36,4 +82,28 @@ function datosCita(e) {
     citaObj[e.target.name] = e.target.value;
 
     console.log(citaObj);
+}
+
+// Valida y agrega nueva cita a la clase de citas
+
+function nuevaCita(e) {
+    e.preventDefault();
+
+    // Extraer la informacion del objeto de cita
+    const { mascota, propietario, telefono, fecha, hora, sintomas } = citaObj;
+
+    // validar
+
+    if( mascota === '' || propietario === '' || telefono === '' || fecha === '' || hora === '' || sintomas === '' ) {
+        ui.imprimirAlerta('Todos los campos son obligatorio', 'error')
+
+        return;
+    }
+
+    // Creando una nueva cita
+
+
+
+
+
 }
